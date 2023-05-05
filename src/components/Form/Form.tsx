4b2c422cb4d89f83e17b4ab13f842647857950ce
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2023-05-05 21:08:39 +0600

    Добавил адаптивную сетку для формы

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,44 +1,56 @@
 function Form() {
   return (
     <form className="form">
-      <label className="form__label" htmlFor="name">
-        Имя:
-      </label>
-      <input className="form__input" type="text" name="name" id="name" placeholder="Введите имя" />
-      <label className="form__label" htmlFor="surname">
-        Фамилия:
-      </label>
-      <input
-        className="form__input"
-        type="text"
-        name="surname"
-        id="surname"
-        placeholder="Введите фамилию"
-      />
-      <label className="form__label" htmlFor="email">
-        Email:*
-      </label>
-      <input
-        className="form__input"
-        type="text"
-        name="email"
-        id="email"
-        required
-        placeholder="Введите email"
-      />
-      <label className="form__label" htmlFor="category">
-        Категория сообщения:*
-      </label>
-      <select className="form__input" name="category" id="category" defaultValue="" required>
-        <option value="" disabled>
-          Выберите категорию
-        </option>
-        <option value="technical">Техническая поддержка</option>
-        <option value="payment">Оплата и доставка</option>
-        <option value="warranty">Возврат и гарантия</option>
-        <option value="reviews">Предложения и отзывы</option>
-        <option value="advertising">Рекламные запросы</option>
-      </select>
+      <div className="form__top-block">
+        <div className="form__left-block">
+          <label className="form__label" htmlFor="name">
+            Имя:
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Введите имя"
+          />
+          <label className="form__label" htmlFor="surname">
+            Фамилия:
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            name="surname"
+            id="surname"
+            placeholder="Введите фамилию"
+          />
+        </div>
+        <div className="form__right-block">
+          <label className="form__label" htmlFor="email">
+            Email:*
+          </label>
+          <input
+            className="form__input"
+            type="text"
+            name="email"
+            id="email"
+            required
+            placeholder="Введите email"
+          />
+          <label className="form__label" htmlFor="category">
+            Категория сообщения:*
+          </label>
+          <select className="form__input" name="category" id="category" defaultValue="" required>
+            <option value="" disabled>
+              Выберите категорию
+            </option>
+            <option value="technical">Техническая поддержка</option>
+            <option value="payment">Оплата и доставка</option>
+            <option value="warranty">Возврат и гарантия</option>
+            <option value="reviews">Предложения и отзывы</option>
+            <option value="advertising">Рекламные запросы</option>
+          </select>
+        </div>
+      </div>
       <label className="form__label" htmlFor="message">
         Сообщение:*
       </label>
